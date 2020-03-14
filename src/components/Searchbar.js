d5354c93c209148a589a547161911a3089fefa8d
codecustard <emmanuelbarroga@gmail.com>
2020-03-14 09:52:53 -0700

    Added search request to Searchbar.

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Searchbar extends Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {value: '', books: ''};
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.searchBook = this.searchBook.bind(this);
     }
 
     handleChange(event) {
@@ -18,11 +20,26 @@ class Searchbar extends Component {
         event.preventDefault();
     }
 
+    searchBook(event) {
+        axios.request( {
+            method: 'get', 
+            url: 'https://www.googleapis.com/books/v1/volumes?q=' + this.state.value
+        }).then( (response) => {
+            this.setState( {books: response.data.items}, () => {
+                console.log(this.state);
+            })
+        }).catch( (error) => {
+            console.log(error);
+        });
+
+        event.preventDefault();
+    }
+
 
     render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.searchBook}>
                     <label>
                     <input type="text" value={this.state.value} onChange={this.handleChange} />
                     </label>
